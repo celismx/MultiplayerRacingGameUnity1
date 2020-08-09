b070528a7abbe6527986f29c090fa217e18b8bb8
celismx <celismx@gmail.com>
2020-08-08 23:59:17 -0500

    first commit

---
+++ Assets/Drive.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+
+public class Drive : MonoBehaviour
+{
+    public WheelCollider WC;
+    public float torque = 200;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        WC = this.GetComponent<WheelCollider>();
+                
+    }
+
+    void Go(float accel)
+    {
+        accel = Mathf.Clamp(accel, -1, 1);
+        float thrustTorque = accel * torque;
+        WC.motorTorque = thrustTorque;
+
+        Quaternion quat;
+        Vector3 position;
+        WC.GetWorldPose(out position, out quat);
+        this.transform.position = position;
+        this.transform.rotation = quat;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        float a = Input.GetAxis("Vertical");
+        Go(a);
+
+    }
+}
